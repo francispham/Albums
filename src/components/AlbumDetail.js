f0ba1f9e510c925d7835f2e4e4c8ecdb510e7846
Francis <francispham89@gmail.com>
2018-08-01 00:41:58 -0700

    Destructuring

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,24 +4,36 @@ import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => (
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyle, headerContentStyle } = styles;
+
+  return (
     <Card>
       <CardSection>
         <View>
-          <Image source={{ uri: props.album.thumbnail_image }} />
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
         </View>
-        <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
   );
+};
 
 const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
 };
 
